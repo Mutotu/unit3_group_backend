cc0c24d1e2fef375d8300225eceda9678bd31a47
arnquist010 <arnquist010@gmail.com>
2021-12-09 18:01:13 -0700

    Completed the '/orders' routes.

--- routers/ordersRouter.js
+++ routers/ordersRouter.js
@@ -1,5 +1,18 @@
 const ordersRouter = require("express").Router();
-
 const ordersController = require("../controllers/ordersController");
 
+// ----------------------------------------
+// | Set routes (base route is '/orders'). |
+// ----------------------------------------
+
+// GET '/' - Return all of the logged-in user's orders.
+ordersRouter.get('/', ordersController.getAllOrders);
+
+// GET '/:orderId' - Return one of the logged-in user's orders.
+ordersRouter.get('/:orderId', ordersController.getOneOrder);
+
+// POST '/new' - Create a new order for the logged-in user.
+ordersRouter.post('/new', ordersController.createOrder);
+
+// Export the userRouter as an express module for use in 'server.js'.
 module.exports = ordersRouter;
