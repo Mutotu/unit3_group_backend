a97569f30db889e0fc4534e935faaa303f2dea31
arnquist010 <arnquist010@gmail.com>
2021-12-12 11:30:03 -0700

    Updated orders table's creditNum column to use a 'BIGINT' data type. Also fixed the order router and controller functions to be compatible with the JWT functionality.

--- routers/ordersRouter.js
+++ routers/ordersRouter.js
@@ -1,4 +1,6 @@
 const ordersRouter = require("express").Router();
+
+const jwtAuth = require('../controllers/middleware/jwtAuth');
 const ordersController = require("../controllers/ordersController");
 
 // ----------------------------------------
@@ -6,13 +8,13 @@ const ordersController = require("../controllers/ordersController");
 // ----------------------------------------
 
 // GET '/' - Return all of the logged-in user's orders.
-ordersRouter.get('/', ordersController.getAllOrders);
+ordersRouter.get('/', [jwtAuth.verifyToken], ordersController.getAllOrders);
 
 // GET '/:orderId' - Return one of the logged-in user's orders.
-ordersRouter.get('/:orderId', ordersController.getOneOrder);
+ordersRouter.get('/:orderId', [jwtAuth.verifyToken], ordersController.getOneOrder);
 
 // POST '/new' - Create a new order for the logged-in user.
-ordersRouter.post('/new', ordersController.createOrder);
+ordersRouter.post('/new', [jwtAuth.verifyToken], ordersController.createOrder);
 
 // Export the userRouter as an express module for use in 'server.js'.
 module.exports = ordersRouter;
